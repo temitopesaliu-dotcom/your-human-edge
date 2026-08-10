0fdfdf6f5923194f48ae42ab74afbdecc5c3082e
temitopesaliu-dotcom <temitope.saliu@gmail.com>
2026-08-10 06:02:53 +0100

    Update rewrites in next.config.ts
    Removed a rewrite rule for '/your-brand-demo'.

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,6 @@ const nextConfig: NextConfig = {
       allowedOrigins: ['temitopesaliu.com', 'www.temitopesaliu.com'],
     },
   },
-  async rewrites() { return [{ source: '/for-lifecoaches', destination: '/for-lifecoaches.html' }, { source: '/your-brand-demo', destination: '/your-brand-demo.html' }, { source: '/get-this-built', destination: '/get-this-built.html' }]; },
+  async rewrites() { return [{ source: '/for-lifecoaches', destination: '/for-lifecoaches.html' }, { source: '/get-this-built', destination: '/get-this-built.html' }]; },
 };
-
 export default nextConfig;
